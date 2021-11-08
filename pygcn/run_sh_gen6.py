07b4cf647dbc8e3904983a6b81f26f3131b37982
recklessronan <380971080@qq.com>
2021-11-08 03:05:20 +0000

    update

--- pygcn/run_sh_gen6.py
+++ pygcn/run_sh_gen6.py
@@ -6,15 +6,15 @@ datasets = ['chameleon', 'cornell', 'squirrel', 'film',
 
 # alpha, beta, gamma, norm_layers, orders, order_func_id, norm_func_id
 best_config = {
-    'chameleon': [1.0, 10000.0, 0.9, 2, 3, 3, 2],
-    'squirrel': [1.0, 10000.0, 0.9, 2, 3, 3, 2],
-    'cora': [1.0, 10000.0, 0.9, 2, 3, 3, 2],
-    'citeseer': [1.0, 10000.0, 0.5, 2, 3, 3, 2],
-    'pubmed': [1.0, 10000.0, 0.9, 2, 3, 3, 2],
-    'texas': [10.0, 1.0, 0.5, 2, 3, 3, 2],
-    'wisconsin': [1.0, 0.1, 0.9, 2, 3, 3, 2],
-    'cornell': [0.5, 0.1, 0.1, 2, 3, 3, 2],
-    'film': [10.0, 10.0, 0.5, 2, 3, 3, 2]
+    'chameleon': [1.0, 10000.0, 0.9, 2, 2, 3, 3],
+    'squirrel': [1.0, 10000.0, 0.9, 2, 2, 3, 3],
+    'cora': [1.0, 10000.0, 0.9, 2, 2, 3, 3],
+    'citeseer': [1.0, 10000.0, 0.5, 2, 2, 3, 3],
+    'pubmed': [1.0, 10000.0, 0.9, 2, 2, 3, 3],
+    'texas': [10.0, 1.0, 0.5, 2, 2, 3, 3],
+    'wisconsin': [1.0, 0.1, 0.9, 2, 2, 3, 3],
+    'cornell': [0.5, 0.1, 0.1, 2, 2, 3, 3],
+    'film': [10.0, 10.0, 0.5, 2, 2, 3, 3]
 }
 
 
@@ -23,11 +23,10 @@ dropout = [0.1, 0.3, 0.5, 0.7, 0.9]
 lr = [0.1, 0.05, 0.01]
 early_stopping = 200
 
-
 run_sh_all = ""
 config_list = []
 for l, d, w in itertools.product(lr, dropout, weight_decay):
-    for data in datasets:
+    for data_index, data in enumerate(datasets):
         best = best_config[data]
         for s in range(10):
             run_sh = "python3 pygcn_raw.py --no-cuda --model mlp_norm --epochs 2000 --hidden 64" + \
@@ -35,12 +34,17 @@ for l, d, w in itertools.product(lr, dropout, weight_decay):
                 " --early_stopping " + str(early_stopping) + \
                 " --dropout " + str(d) + " --alpha " + str(best[0]) + \
                 " --beta " + str(best[1]) + ' --gamma ' + str(best[2]) + \
-                " --norm_layers " + str(best[3]) + " --orders " + str(best[4]) + \
-                " --orders_func_id " + str(best[5]) + " --norm_func_id " + str(best[6]) + \
+                " --norm_layers " + str(best[4]) + " --orders " + str(best[6]) + \
+                " --orders_func_id " + str(best[5]) + " --norm_func_id " + str(best[3]) + \
                 " --dataset " + data + " --split " + str(s)
             run_sh_all += run_sh + '\n'
-        config = [l, w, early_stopping, w].extend(best)
+        # print('best', best)
+        # print(l, d, early_stopping, w)
+        config = [l, d, early_stopping, w]
+        config.extend(best)
+        config.append(data_index)
         config_list.append(config)
+    # print('config', config)
 
 with open('config_list6', 'wb') as f:
     pickle.dump(config_list, f)

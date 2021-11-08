1e66e1269bf4d07f8bfed6cf02bfc22b469367fb
recklessronan <380971080@qq.com>
2021-11-08 13:32:06 +0000

    update

--- pygcn/pygcn_raw.py
+++ pygcn/pygcn_raw.py
@@ -17,6 +17,8 @@ import os
 import json
 import warnings
 
+from ppnp.ppnp.pytorch import training
+
 warnings.filterwarnings('ignore')
 
 torch.set_default_dtype(torch.float64)
@@ -144,6 +146,7 @@ class MLP_NORM(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.fc2(x)
         h0 = x
+        adj = F.dropout(adj, self.dropout, training=self.training)
         for _ in range(self.norm_layers):
             x = self.norm(x, h0, adj)
         return F.log_softmax(x, dim=1)

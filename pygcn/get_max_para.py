3b50b9a9384923355d85acd5f74a45a159bd2891
recklessronan <380971080@qq.com>
2021-11-13 06:29:24 +0000

    update

---
+++ pygcn/get_max_para.py
@@ -0,0 +1,75 @@
+# %%
+import pickle
+import pandas as pd
+import os
+
+# %%
+with open('config_list1', 'rb') as f:
+    config_list = pickle.load(f)
+non_para = 0
+exist_para = 0
+
+datasets = ['chameleon', 'cornell', 'squirrel', 'film',
+            'texas', 'wisconsin', 'pubmed', 'cora', 'citeseer']
+config_str = ['lr', 'do', 'es', 'wd', 'alpha', 'beta',
+              'gamma', 'nlid', 'nl', 'ordersid', 'orders']
+
+# %%
+
+# %%
+print('the number of all parameters', len(config_list))
+results_all = [[] for _ in range(len(config_str) + 2)]
+for config in config_list:
+    acc_res = []
+    for i in range(10):
+        url_configs = ''
+        for j in range(len(config_str)):
+            url_configs += '_' + config_str[j] + str(config[j])
+        url = 'runs/' + datasets[config[-1]] + url_configs + \
+            '_split' + str(i) + '_results.txt'
+        # print(url)
+        if os.path.exists(url):
+            # print('url exists')
+            with open(url, 'r') as f:
+                s = f.read()
+                sub1 = '"test_acc": '
+                sub2 = ', "test_duration"'
+                result = s[s.index(sub1) + len(sub1): s.index(sub2)]
+                acc_res.append(float(result))
+    # print(url)
+    # print(len(acc_res))
+    if len(acc_res) == 10:
+        exist_para += 1
+        results_all[0].append(datasets[config[-1]])
+        for i in range(len(config_str)):
+            results_all[i+1].append(config[i])
+        results_all[-1].append(sum(acc_res)/len(acc_res))
+    else:
+        non_para += 1
+        # print(url)
+
+print('the number of non-existing parameters', non_para)
+print('the number of existing parameters', exist_para)
+
+# %%
+all_config_str = ['dataset', 'lr', 'dropout', 'early_stopping', 'weight_decay',
+                  'alpha', 'beta', 'gamma', 'norm_func_id', 'norm_layers',
+                  'orders_func_id', 'orders', 'accuracy']
+d = {}
+assert len(all_config_str) == len(results_all)
+for i in range(len(all_config_str)):
+    d[all_config_str[i]] = results_all[i]
+
+df = pd.DataFrame.from_dict(d)
+df = df.sort_values(['dataset', 'accuracy']).reset_index(drop=True)
+
+csv_name = 'mlpnorm_result_' + df['dataset'][0] + '.csv'
+# if os.path.exists(csv_name):
+#     df.to_csv(csv_name, mode='a', header=False)
+# else:
+#     df.to_csv(csv_name)
+
+# %%
+
+
+# %%
--- pygcn/run_sepsh_gen.py
+++ pygcn/run_sepsh_gen.py
@@ -2,7 +2,8 @@ import pickle
 import itertools
 
 # dataset = 'texas'
-dataset = 'chameleon'
+# dataset = 'chameleon'
+dataset = 'film'
 
 datasets = ['chameleon', 'cornell', 'squirrel', 'film',
             'texas', 'wisconsin', 'pubmed', 'cora', 'citeseer']
@@ -11,16 +12,29 @@ for i in range(len(datasets)):
     if datasets[i] == dataset:
         data_id = i
 
+
+# best_config = {
+#     'chameleon': [0.01, 0.3, 250, 0.00005, 1.0, 100000000.0, 0.1, 1, 2, 3, 3],
+#     'squirrel': [0.05, 0.1, 250, 0.00001, 0.0, 1000000.0, 0.1, 1, 2, 3, 3],
+#     'cora': [0.01, 0.6, 40, 0.00001, 1000000.0, 10000.0, 0.7, 1, 2, 3, 4],
+#     'citeseer': [0.01, 0.6, 40, 0.0000001, 100.0, 100.0, 0.9, 1, 2, 3, 5],
+#     'pubmed': [0.01, 0.2, 40, 0.00005, 1000000.0, 1000000.0, 0.5, 1, 2, 3, 1],
+#     'texas': [0.1, 0.1, 200, 0.0001, 10000.0, 10.0, 0.8, 1, 2, 3, 3],
+#     'wisconsin': [0.05, 0.1, 40, 0.0001, 10.0, 0.05, 0.9, 1, 2, 3, 3],
+#     'cornell': [0.05, 0.0, 40, 0.00005, 1.0, 0.1, 0.6, 1, 2, 3, 3],
+#     'film': [0.001, 0.0, 40, 0.001, 10.0, 1.0, 10.0, 1, 2, 3, 4]
+# }
+
 best_config = {
-    'chameleon': [0.01, 0.3, 250, 0.00005, 1.0, 100000000.0, 0.1, 1, 2, 3, 3],
-    'squirrel': [0.05, 0.1, 250, 0.00001, 0.0, 1000000.0, 0.1, 1, 2, 3, 3],
-    'cora': [0.01, 0.6, 40, 0.00001, 1000000.0, 10000.0, 0.7, 1, 2, 3, 4],
-    'citeseer': [0.01, 0.6, 40, 0.0000001, 100.0, 100.0, 0.9, 1, 2, 3, 5],
-    'pubmed': [0.01, 0.2, 40, 0.00005, 1000000.0, 1000000.0, 0.5, 1, 2, 3, 1],
-    'texas': [0.1, 0.1, 200, 0.0001, 10000.0, 10.0, 0.8, 1, 2, 3, 3],
-    'wisconsin': [0.05, 0.1, 40, 0.0001, 10.0, 0.05, 0.9, 1, 2, 3, 3],
-    'cornell': [0.05, 0.0, 40, 0.00005, 1.0, 0.1, 0.6, 1, 2, 3, 3],
-    'film': [0.001, 0.0, 40, 0.001, 10.0, 10.0, 0.2, 1, 2, 3, 3]
+    'chameleon': [0.01, 0.3, 250, 0.00005, 1.0, 100000000.0, 0.1, 2, 2, 3, 3],
+    'squirrel': [0.05, 0.1, 250, 0.00001, 0.0, 1000000.0, 0.1, 2, 2, 3, 3],
+    'cora': [0.01, 0.6, 40, 0.00001, 1000000.0, 10000.0, 0.7, 2, 2, 3, 4],
+    'citeseer': [0.01, 0.6, 40, 0.0000001, 100.0, 100.0, 0.9, 2, 2, 3, 5],
+    'pubmed': [0.01, 0.2, 40, 0.00005, 1000000.0, 1000000.0, 0.5, 2, 2, 3, 1],
+    'texas': [0.1, 0.1, 200, 0.0001, 10000.0, 10.0, 0.8, 2, 2, 3, 3],
+    'wisconsin': [0.05, 0.1, 40, 0.0001, 10.0, 0.05, 0.9, 2, 2, 3, 3],
+    'cornell': [0.05, 0.0, 40, 0.00005, 1.0, 0.1, 0.6, 2, 2, 3, 3],
+    'film': [0.001, 0.0, 40, 0.001, 0.1, 0.1, 0.2, 2, 2, 3, 2]
 }
 
 best = best_config[dataset]
@@ -32,26 +46,27 @@ weight_decay = [0.000005, 0.0005]
 dropout = [0.3, 0.7]
 early_stopping = [250]
 gamma = [0.3, 0.7]
-orders = [1, 3]
+orders = [2, 3, 4]
 norm_layers = [1, 2]
-alpha = [1.0]
-beta = [0.1, 1000000.0]
+alpha = [0.1, 1.0, 10.0]
+beta = [0.1, 1.0, 10.0]
 
 
-# for a, b in itertools.product(alpha, beta):
-#     best[4] = a
-#     best[5] = b
+for a, b, o in itertools.product(alpha, beta, orders):
+    best[4] = a
+    best[5] = b
+    best[10] = o
 
 #     if a+b == 0.0:
 #         continue
 
-for l, d, e, w, g, o in itertools.product(lr, dropout, early_stopping, weight_decay, gamma, orders):
-    best[0] = l
-    best[1] = d
-    best[2] = e
-    best[3] = w
-    best[6] = g
-    best[10] = o
+# for l, d, e, w, g, o in itertools.product(lr, dropout, early_stopping, weight_decay, gamma, orders):
+#     best[0] = l
+#     best[1] = d
+#     best[2] = e
+#     best[3] = w
+#     best[6] = g
+#     best[10] = o
 
     for s in range(10):
         run_sh = "python3 pygcn_raw.py --no-cuda --model mlp_norm --epochs 2000 --hidden 80" + \

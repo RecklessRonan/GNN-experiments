0589de29541c07dafa27b8f598bce04abd29dfc4
recklessronan <380917080@qq.com>
2021-11-17 21:14:14 +0800

    update

--- Non-Homophily-Large-Scale/experiments/mlpnorm_exp.sh
+++ Non-Homophily-Large-Scale/experiments/mlpnorm_exp.sh
@@ -6,15 +6,20 @@ startTime_s=`date +%s`
 dataset=$1
 sub_dataset=${2:-''}
 
-lr_lst=(0.01)
+lr_lst=(0.005)
 hidden_channels_lst=(256)
 dropout_lst=(0.0)
 weight_decay_lst=(0.0)
 alpha_lst=(1.0)
-beta_lst=(0.1 10.0 1000.0)
-gamma_lst=(0.1 0.5 0.9)
+beta_lst=(10)
+gamma_lst=(0.9)
 norm_layers_lst=(2)
-orders_lst=(1 2 3)
+orders_lst=(2)
+epochs=5000
+runs=5
+norm_func_id=2
+order_func_id=2
+
 
 for lr in "${lr_lst[@]}"; do
     for hidden_channels in "${hidden_channels_lst[@]}"; do
@@ -26,9 +31,9 @@ for lr in "${lr_lst[@]}"; do
                             for orders in "${orders_lst[@]}"; do
                                 for alpha in "${alpha_lst[@]}"; do
                                     if [ "$dataset" = "snap-patents" ] || [ "$dataset" = "arxiv-year" ]; then
-                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs 1000 --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id 2 --norm_layers $norm_layers --orders_func_id 2 --orders $orders --display_step 25 --runs 1 --directed
+                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 25 --runs $runs --directed
                                     else
-                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs 1000  --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id 2 --norm_layers $norm_layers --orders_func_id 2 --orders $orders --display_step 25 --runs 1
+                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 25 --runs $runs
                                     fi
                                 done
                             done
--- Non-Homophily-Large-Scale/run_all.sh
+++ Non-Homophily-Large-Scale/run_all.sh
@@ -1,8 +1,10 @@
+# bash experiments/mlpnorm_exp.sh snap-patents
+bash experiments/mlpnorm_exp.sh pokec
 bash experiments/mlpnorm_exp.sh twitch-gamer
-bash experiments/mlpnorm_exp.sh fb100 Penn94
+# bash experiments/mlpnorm_exp.sh fb100 Penn94
 bash experiments/mlpnorm_exp.sh arxiv-year
 bash experiments/mlpnorm_exp.sh genius
-bash experiments/mlpnorm_exp.sh pokec
-bash experiments/mlpnorm_exp.sh snap-patents
+
+
 
 

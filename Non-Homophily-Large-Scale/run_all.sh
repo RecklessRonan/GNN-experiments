18657a1d2c301eda48a4cb082986d14d2f875359
recklessronan <380917080@qq.com>
2021-11-20 09:55:58 +0800

    update

--- Non-Homophily-Large-Scale/run_all.sh
+++ Non-Homophily-Large-Scale/run_all.sh
@@ -1,9 +1,9 @@
-# bash experiments/mlpnorm_exp.sh snap-patents
-# bash experiments/mlpnorm_exp.sh pokec
-bash experiments/mlpnorm_exp.sh fb100 Penn94
-bash experiments/mlpnorm_exp.sh twitch-gamer
-bash experiments/mlpnorm_exp.sh arxiv-year
-bash experiments/mlpnorm_exp.sh genius
+# bash experiments/mlpnorm_exp_snap.sh snap-patents
+# bash experiments/mlpnorm_exp_pokec.sh pokec
+bash experiments/mlpnorm_exp_penn.sh fb100 Penn94
+bash experiments/mlpnorm_exp_twitch.sh twitch-gamer
+bash experiments/mlpnorm_exp_arxiv.sh arxiv-year
+bash experiments/mlpnorm_exp_genius.sh genius
 
 
 

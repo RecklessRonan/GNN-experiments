1fb754c919c6b5239e1a18767e40314c4678ac07
recklessronan <380971080@qq.com>
2021-11-26 03:32:00 +0000

    update

--- Non-Homophily-Large-Scale/experiments/mlpnorm_exp_snap.sh
+++ Non-Homophily-Large-Scale/experiments/mlpnorm_exp_snap.sh
@@ -6,13 +6,13 @@ startTime_s=`date +%s`
 dataset=snap-patents
 sub_dataset=${2:-''}
 
-lr_lst=(0.01)
+lr_lst=(0.025)
 hidden_channels_lst=(128)
 dropout_lst=(0.0)
 weight_decay_lst=(0.0)
-alpha_lst=(1.0)
-beta_lst=(10.0 100.0 1000.0)
-gamma_lst=(0.1 0.5 0.9)
+alpha_lst=(1)
+beta_lst=(1000.0)
+gamma_lst=(0.9)
 norm_layers_lst=(2)
 orders_lst=(3)
 epochs=2000
@@ -31,9 +31,9 @@ for lr in "${lr_lst[@]}"; do
                             for orders in "${orders_lst[@]}"; do
                                 for alpha in "${alpha_lst[@]}"; do
                                     if [ "$dataset" = "snap-patents" ] || [ "$dataset" = "arxiv-year" ]; then
-                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 25 --runs $runs --directed
+                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 5 --runs $runs --directed
                                     else
-                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 25 --runs $runs
+                                        python -u main.py --dataset $dataset --sub_dataset ${sub_dataset:-''} --method mlpnorm --epochs $epochs --hidden_channels $hidden_channels --lr $lr --dropout $dropout --weight_decay $weight_decay --alpha $alpha --beta $beta --gamma $gamma --norm_func_id $norm_func_id --norm_layers $norm_layers --orders_func_id $order_func_id --orders $orders --display_step 5 --runs $runs
                                     fi
                                 done
                             done
